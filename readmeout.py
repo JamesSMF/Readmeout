b9a69b14d7b92631173d93cef6962d39e533934f
江南吴彦祖 <gli38@ucsc.edu>
2020-02-02 14:38:45 -0800

    auto-update by my python script

--- readmeout.py
+++ readmeout.py
@@ -6,6 +6,7 @@ with open("to_be_read.txt", "rt") as f:
    with open("out_text.txt", "wt") as fo:
       for line in f:
          re.sub('[\'\"<>~`]','',line)
+         re.sub('- ','',line)
          fo.write(line)
 
 # Open the file
